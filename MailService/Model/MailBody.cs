5dfc2fdd05afb5562c71fee6a6d638b9df9af692
Narender Kumar <Narender.Kumar@multichoice.co.za>
2022-09-13 13:49:00 +0530

    Version 1.1 Complete

---
+++ MailService/Model/MailBody.cs
@@ -0,0 +1,29 @@
+﻿namespace MailService.Model
+{
+    using MailService.Interface;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Net.Mail;
+
+    public class MailBody : IMailBody
+    {
+        [Required]
+        public MailAddress SenderAddress { get; set; }
+        [Required]
+        public List<MailAddress> ToList { get; set; }
+        public List<MailAddress> CcList { get; set; }
+        public List<MailAddress> BccList { get; set; }
+        public MailPriority EMailPriority { get; set; } = MailPriority.Normal;
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public int SmtpClientPort { get; set; }
+        [Required]
+        public string SmtpClientHost { get; set; }
+        [Required]
+        public string SmtpClientPassword { get; set; }
+        public bool IsBodyHtml { get; set; } = true;
+    }
+}
